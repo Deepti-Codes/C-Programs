15015e40a5193ab3b1c623b7974840b4c709b5c1
Deepti-Codes <singhdeepti0406@gmail.com>
2026-01-12 23:17:12 +0530

    Create even odd.cpp

---
+++ even odd.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+uisng namespace std;
+
+int main()
+{
+  int num;
+  cout<<"Enter a number:"<<endl;
+  cin>>num;
+
+  if(num%2==0)
+{  cout<<"Even number"<<endl;
+}
+  else
+{  cout<<"Odd number"<<endl;
+}
+return 0;
+
+}
+   
